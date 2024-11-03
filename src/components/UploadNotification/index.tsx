5c8cfac6b59173453b86d94b56b832d6ce61a53d
Minh Minh <nguyenthiminhminh6a1@gmail.com>
2024-11-04 00:10:24 +0700

    feat: noti pop up setup

--- src/components/UploadNotification/index.tsx
+++ src/components/UploadNotification/index.tsx
@@ -15,12 +15,26 @@ const UploadNotification: FC<UploadNotificationProps> = ({isOpen , uploadStatus 
     const isSuccess = uploadStatus === 'success';
 
     return (
-        <Dialog open={false}>
+        <Dialog open={isOpen}
+            sx={{
+                backgroundColor: isSuccess ? theme.palette.success.main : theme.palette.error.main,
+                color: theme.palette.common.white,
+                padding: theme.spacing(2),
+                borderRadius: theme.spacing(1),
+                textAlign: 'center',
+                position: 'absolute',
+                width: '50rem',
+                height: '10rem',
+                top: '50%',
+                left: '50%',
+            }}
+            onClose={onClose}
+        >
             hi
             
         </Dialog>
 
-    )
-}
+    );
+};
 
 export default UploadNotification;
